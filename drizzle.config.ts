0b8c764b62eca0e9ee1744714a669eec677c9860
Louis Rouanet <rouanetflorian@gmail.com>
2025-09-17 14:34:01 +0200

    drizzle and bdd push preprod, deploy

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,16 @@
+// drizzle.config.ts
+import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
+  dialect: "postgresql",
   schema: "./src/server/db/schema.ts",
   out: "./drizzle",
-  dialect: "postgresql",
+  // ⬇️ 0.21+ : url ; 0.20- : connectionString
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,              // si 0.21+
+    // connectionString: process.env.DATABASE_URL!, // si 0.20-
   },
+  strict: true,
+  verbose: true,
 });
